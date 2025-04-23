c4c4cb6566a6e2b47059f6a593af4a0fed960684
Ashutosh kumar <110932917+Pulsekein@users.noreply.github.com>
2025-04-23 23:18:58 +0530

    Update App.js

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ const DataBase = require("./Database");
 const app = express();
 
 app.use(cors({
-    origin: ["http://localhost:3000"],
+    origin: ["https://advancetaker.netlify.app"],
     methods: ["GET", "POST", "DELETE", "PUT", "PATCH"],
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"]
